7a834d1c7f7866f3092c5ff27ae0af508783038f
luciferonlylegend830 <luciferonlylegend830@gmail.com>
2026-07-22 10:00:31 +0400

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -2,17 +2,16 @@ from telethon import TelegramClient
 import json
 
 # अपनी डिटेल्स यहाँ भरें (my.telegram.org से लें)
-api_id = '36304765'      # यहाँ अपना API ID डालें
+api_id = '36304765'  # यहाँ अपना API ID डालें
 api_hash = '449c24a0a125276520b2b0bf3d93bf2c'  # यहाँ अपना API HASH डालें
 client = TelegramClient('anon', api_id, api_hash)
 
 async def main():
     # चैनल की ID डालें (-1002748829128)
-    channel_username = -1002748829128 
+    channel_username = -1002748829128
     movies = {}
-        # चैनल के सभी मैसेज को स्कैन करेगा
-print("Scanning channel...")
     # चैनल के सभी मैसेज को स्कैन करेगा
+    print("Scanning channel...")
     async for message in client.iter_messages(channel_username):
         if message.video or message.document:
             caption_text = message.caption or message.text or ""
@@ -25,8 +24,6 @@ print("Scanning channel...")
                 "file_name": file_name
             }
             print(f"Added: {file_name}")
-            
-            
 
     # सारी मूवीज़ को एक फाइल में सेव करेगा
     with open('movies.json', 'w') as f:
@@ -35,4 +32,4 @@ print("Scanning channel...")
 
 with client:
     client.loop.run_until_complete(main())
-  
+    
